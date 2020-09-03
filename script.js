21bdfd58d6b354007f0ecd3f96e6d302e1a44ac2
Alexander Sjögren <alle656@hotmail.com>
2020-09-03 15:47:05 +0200

    Klar med uppgiften

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const btn = document.getElementById("btn");
 const input1 = document.getElementById("input1");
 const input2 = document.getElementById("input2");
+
 let input1Value = 0;
 let input2Value = 0;
 
@@ -19,20 +20,30 @@ function selection() {
         case "+":
             let additionResult = input1ValueInt + input2ValueInt;
             console.log(additionResult);
+            fieldOutput(input1Value + " + "  + input2Value + " = " + additionResult);
             break;
         case "-":
             let subtractionResult = input1ValueInt - input2ValueInt;
             console.log(subtractionResult);
+            fieldOutput(input1Value + " - "  + input2Value + " = " + subtractionResult);
             break;
         case "/":
             let divisionResult = input1ValueInt / input2ValueInt
             console.log(divisionResult);
+            fieldOutput(input1Value + " / "  + input2Value + " = " + divisionResult);
             break;
         case "*":
             let multiplicationResult = input1ValueInt * input2ValueInt;
             console.log(multiplicationResult);
+            fieldOutput(input1Value + " * "  + input2Value + " = " + multiplicationResult);
             break;
     }
 }
 
-
+function fieldOutput (test, resultat) {
+    const newLegend = document.createElement("legend");
+    const legendOutput = document.createTextNode(test, resultat);
+    newLegend.appendChild(legendOutput);
+    const output = document.getElementById("output");
+    output.appendChild(newLegend);
+}
